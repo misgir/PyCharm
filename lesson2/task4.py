aaf6137b1c1bc5371ed37282a44beab4f2b6e0e6
pinchukalinaivanovna <alinapinchuk20@dmail.com>
2024-11-22 16:41:19 +0700

    Конечный вариант(?)

--- lesson2/task4.py
+++ lesson2/task4.py
@@ -3,12 +3,15 @@ n = int(input("fizz_buzz:"))
 
 def fizz_buzz(n):
     for n in range(1,n + 1):
-        if n % 3 == 0:
+        if n % 3 == 0 and n % 5 == 0:
+            print(f"{n} - FizzBuzz")
+        elif n % 3 == 0:
             print(f"{n} - Fizz")
         elif n % 5 == 0:
             print(f"{n} - buzz")
-        else
-            print(
+
+
+print(f"{n} - fizz_buzz")
 
 
 
