bde0a0123549b86860e13231cd4524c760f92ff5
pinchukalinaivanovna <alinapinchuk20@gmail.com>
2025-01-30 19:43:18 +0700

    k/r

--- lessson_6_K/test_01_form.py
+++ lessson_6_K/test_01_form.py
@@ -1,76 +1,46 @@
-from selenium.webdriver import Chrome
-from selenium.webdriver.remote.webelement import WebElement
-from selenium.webdriver.support.color import Color
+from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
 
+browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
-def find_element_by(browser: Chrome, by: By, path: str) -> WebElement:
-    return browser.find_element(by, path)
+browser.get("https://bonigarcia.dev/selenium-webdriver-java/data-types.html")
 
+name = "body > main > div > form > div:nth-child(1) > div:nth-child(1) > label > input"
 
-def test_fields_color() -> None:
-    browser = Chrome(service=ChromeService(ChromeDriverManager().install()))
-    browser.get("https://bonigarcia.dev/selenium-webdriver-java/data-types.html")
+first_name = browser.find_element(By.CSS_SELECTOR, name)
 
-    first_name_id = "first-name"
-    last_name_id = "last-name"
-    address_id = "address"
-    zip_id = "zip-code"
-    city_id = "city"
-    country_id = "country"
-    email_id = "e-mail"
-    phone_id = "phone"
-    job_id = "job-position"
-    company_id = "company"
+first_name.send_keys("Иван")
 
-    find_element_by(browser, By.NAME, first_name_id).send_keys("Иван")
-    find_element_by(browser, By.NAME, last_name_id).send_keys("Петров")
-    find_element_by(browser, By.NAME, address_id).send_keys("Ленина, 55-3")
-    find_element_by(browser, By.NAME, zip_id).send_keys("")
-    find_element_by(browser, By.NAME, city_id).send_keys("Москва")
-    find_element_by(browser, By.NAME, country_id).send_keys("Россия")
-    find_element_by(browser, By.NAME, email_id).send_keys("test@skypro.com")
-    find_element_by(browser, By.NAME, phone_id).send_keys("+7985899998787")
-    find_element_by(browser, By.NAME, job_id).send_keys("QA")
-    find_element_by(browser, By.NAME, company_id).send_keys("SkyPro")
+name2 = "body > main > div > form > div:nth-child(1) > div:nth-child(2) > label > input"
 
-    button_path = "body > main > div > form > div:nth-child(5) > div > button"
-    button = browser.find_element(By.CSS_SELECTOR, button_path)
-    ActionChains(browser).move_to_element(button).perform()
-    button.click()
+last_name = browser.find_element(By.CSS_SELECTOR, name2).send_keys("Петров")
+name3 = "body > main > div > form > div:nth-child(2) > div.col-md-4.py-2 > label > input"
 
-    first_name = find_element_by(browser, By.ID, first_name_id)
-    last_name = find_element_by(browser, By.ID, last_name_id)
-    address = find_element_by(browser, By.ID, address_id)
-    zip_code = find_element_by(browser, By.ID, zip_id)
-    city = find_element_by(browser, By.ID, city_id)
-    country = find_element_by(browser, By.ID, country_id)
-    email = find_element_by(browser, By.ID, email_id)
-    phone = find_element_by(browser, By.ID, phone_id)
-    job = find_element_by(browser, By.ID, job_id)
-    company = find_element_by(browser, By.ID, company_id)
+address = browser.find_element(By.CSS_SELECTOR, name3).send_keys("Ленина, 55-3")
+name4 = "body > main > div > form > div:nth-child(2) > div.col-md-2.py-2 > label > input"
 
-    green_color = "#0f5132"
-    red_color = "#842029"
+zip = browser.find_element(By.CSS_SELECTOR, name4).send_keys("")
+name5 = "body > main > div > form > div:nth-child(2) > div:nth-child(3) > label > input"
 
-    assert Color.from_string(zip_code.value_of_css_property("color")).hex == red_color
-    assert (
-        Color.from_string(first_name.value_of_css_property("color")).hex
-        == Color.from_string(first_name.value_of_css_property("color")).hex
-        == Color.from_string(last_name.value_of_css_property("color")).hex
-        == Color.from_string(address.value_of_css_property("color")).hex
-        == Color.from_string(city.value_of_css_property("color")).hex
-        == Color.from_string(country.value_of_css_property("color")).hex
-        == Color.from_string(email.value_of_css_property("color")).hex
-        == Color.from_string(phone.value_of_css_property("color")).hex
-        == Color.from_string(job.value_of_css_property("color")).hex
-        == Color.from_string(company.value_of_css_property("color")).hex
-        == green_color
-    )
+city = browser.find_element(By.CSS_SELECTOR, name5).send_keys("Москва")
+name6 = "body > main > div > form > div:nth-child(2) > div:nth-child(4) > label > input"
 
+country = browser.find_element(By.CSS_SELECTOR, name6).send_keys("Россия")
+name7 = "body > main > div > form > div:nth-child(3) > div:nth-child(1) > label > input"
 
-if name == "__main__":
-    test_fields_color()
+email = browser.find_element(By.CSS_SELECTOR, name7).send_keys("test@skypro.com")
+name8 = "body > main > div > form > div:nth-child(3) > div:nth-child(2) > label > input"
+
+phone_num = browser.find_element(By.CSS_SELECTOR, name8).send_keys("+7985899998787")
+name9 = "body > main > div > form > div:nth-child(4) > div:nth-child(1) > label > input"
+
+job_position = browser.find_element(By.CSS_SELECTOR, name9).send_keys("QA")
+name10 = "body > main > div > form > div:nth-child(4) > div:nth-child(2) > label > input"
+
+company = browser.find_element(By.CSS_SELECTOR, name10).send_keys("SkyPro")
+
+button = "body > main > div > form > div:nth-child(5) > div > button"
+
+submit = browser.find_element(By.CSS_SELECTOR, button).click()
