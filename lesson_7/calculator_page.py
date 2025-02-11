3d93e947ec0978a78f7459d9d3761c883bc7ba66
pinchukalinaivanovna <alinapinchuk20@gmail.com>
2025-02-11 23:15:25 +0700

    comment

--- lesson_7/calculator_page.py
+++ lesson_7/calculator_page.py
@@ -1,3 +1,4 @@
+import allure
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -11,21 +12,26 @@ class CalculatorPage(BasePage):
         super().__init__(driver)
         self.url = "https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html"
 
+    @allure.step('Открываем сайт')
     def open(self):
          self._driver.get(self.url)
 
+    @allure.step('В поле ввода по локатору delay вводим значение 45')
     def set_delay(self, delay):
         delay_input = self._driver.find_element(By.CSS_SELECTOR, "input#delay")
         delay_input.clear()
         delay_input.send_keys(delay)
 
+    @allure.step('Нажимаем на кнопку')
     def button(self, button_text):
         button = self._driver.find_element(By.XPATH, f'//span[text()="{button_text}"]')
         button.click()
 
+    @allure.step('Получаем результат')
     def result(self):
          return self._driver.find_element(By.CSS_SELECTOR, ".screen").text
 
+    @allure.step('Проверяем что в окне отобразится результат 15 через 45 секунд')
     def wait_for_result(self, expected_result):
          WebDriverWait(self._driver, 50).until(
             EC.text_to_be_present_in_element((By.CSS_SELECTOR, ".screen"), expected_result)
